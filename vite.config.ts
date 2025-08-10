9067b45cca0fc41473a27194194837409cc29fbd
bastianplsfix <sebastian.nomme@icloud.com>
2025-08-10 21:24:30 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart({
+      customViteReactPlugin: true,
+      target: "node-server",
+    }),
+    react(),
+    tailwindcss(),
+  ],
+});
