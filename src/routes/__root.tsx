c8a3fc73f3b4ea66e4965abf853ebc8d46d7a99f
bastianplsfix <sebastian.nomme@icloud.com>
2025-08-11 09:00:03 +0200

    added clerk

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,17 +3,49 @@
 import type { ReactNode } from "react";
 import {
   Outlet,
-  createRootRoute,
   HeadContent,
   Scripts,
+  createRootRouteWithContext,
+  useRouteContext,
 } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useAuth,
+} from "@clerk/tanstack-react-start";
 import { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext } from "@tanstack/react-router";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+
+import { getAuth } from "@clerk/tanstack-react-start/server";
+import { getWebRequest } from "@tanstack/react-start/server";
+
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 import css from "src/assets/global.css?url";
 
+const fetchClerkAuth = createServerFn({ method: "GET" }).handler(async () => {
+  const request = getWebRequest();
+
+  if (!request) throw new Error("No request found");
+
+  const auth = await getAuth(request);
+  const token = await auth.getToken({ template: "convex" });
+
+  return {
+    userId: auth.userId,
+    token,
+  };
+});
+
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
+  convexClient: ConvexReactClient;
+  convexQueryClient: ConvexQueryClient;
 }>()({
   head: () => ({
     meta: [
@@ -30,14 +62,35 @@ export const Route = createRootRouteWithContext<{
     ],
     links: [{ rel: "stylesheet", href: css }],
   }),
+  beforeLoad: async (ctx) => {
+    const auth = await fetchClerkAuth();
+    const { userId, token } = auth;
+
+    // During SSR only (the only time serverHttpClient exists),
+    // set the Clerk auth token to make HTTP queries with.
+    if (token) {
+      ctx.context.convexQueryClient.serverHttpClient?.setAuth(token);
+    }
+
+    return {
+      userId,
+      token,
+    };
+  },
   component: RootComponent,
 });
 
 function RootComponent() {
+  const context = useRouteContext({ from: Route.id });
+
   return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={context.convexClient} useAuth={useAuth}>
+        <RootDocument>
+          <Outlet />
+        </RootDocument>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 }
 
@@ -48,6 +101,15 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
         <HeadContent />
       </head>
       <body>
+        <div className="ml-auto">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal" />
+          </SignedOut>
+        </div>
+        <hr />
         {children}
         <Scripts />
       </body>
